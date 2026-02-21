a8790ae857b050c33f2401b665d7b9415436dd97
fqzlr <a10275974@outlook.com>
2026-02-21 22:25:27 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -58,14 +58,14 @@ export default {
     { text: '昔日', link: '/archives', icon: 'Nav_archives' },
     { text: '留言', link: '/message', icon: 'Nav_message' },
     { text: '关于', link: '/about', icon: 'Nav_about' },
-    { text: 'API', link: 'https://api.vvhan.com/', target: true, icon: 'Nav_link' },
+    { text: 'home', link: 'https://home.682000.xyz', target: true, icon: 'Nav_link' },
   ],
   // 侧边栏个人网站
   WebSites: [
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
-    { text: 'Github', link: 'https://github.com/fqzlr', icon: 'WebSite_github' },
     { text: 'fqzlrのhome', link: 'https://home.682000.xyz', icon: 'WebSite_home' },
+    { text: 'Github', link: 'https://github.com/fqzlr', icon: 'WebSite_github' },
     { text: 'fqzlrの图床', link: 'https://tu.fqzlr.top', icon: 'WebSite_img' },
   ],
   // 侧边栏展示
